603e7dc72a6031659f87ca4c235b26e836cbaf30
viki <vikionenine@gmail.com>
2024-09-09 01:06:14 +0530

    add  header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Dashboard from "./pages/Dashboard"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Projects from "./pages/Projects"
+import Header from "./components/Header"
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 
   return (
    <BrowserRouter >
+   <Header/>
    <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/about' element={<About/>}/>
